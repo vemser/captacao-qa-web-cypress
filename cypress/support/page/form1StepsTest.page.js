996d56c66226ec1d0e2567b059dffd72a7827a0f
Aron Adams <aronrapetto@hotmail.com>
2024-03-04 03:55:57 -0300

    test: casos negativos do primeiro formulario de inscrição

--- cypress/support/page/form1StepsTest.page.js
+++ cypress/support/page/form1StepsTest.page.js
@@ -2,26 +2,56 @@ import formStep from "../selector/formSteps.selector"
 import cadastro from "../data/cadastroCandidatoPasso1"
 
 Cypress.Commands.add("preencherTodosCamposComSucesso", () => {
-    cy.get(formStep.campoNome).type(cadastro.nome)
-    cy.get(formStep.campoEmail).type(cadastro.email)
-    cy.get(formStep.campoRg).type(cadastro.rg)
-    cy.get(formStep.campoCpf).type(cadastro.cpf)
-    cy.get(formStep.campoTelefone).type(cadastro.telefone)
-    cy.get(formStep.campoDataNascimento).type(cadastro.dataNascimento)
-    cy.get(formStep.campoCidade).type(cadastro.cidade)
+    const dados = cadastro()
+
+    cy.get(formStep.campoNome).type(dados.nome)
+    cy.get(formStep.campoEmail).type(dados.email)
+    cy.get(formStep.campoRg).type(dados.rg)
+    cy.get(formStep.campoCpf).type(dados.cpf)
+    cy.get(formStep.campoTelefone).type(dados.telefone)
+    cy.get(formStep.campoDataNascimento).type(dados.dataNascimento)
+    cy.get(formStep.campoCidade).type(dados.cidade)
 
     cy.get(formStep.campoEstado).click()
-    cy.get(formStep.opcoesEstado).eq(cadastro.estado).click()
+    cy.get(formStep.opcoesEstado).eq(dados.estado).click()
 
     cy.get(formStep.campoNeurodiversidade).click()
-    cy.get(formStep.opcoesNeurdiversidade).eq(cadastro.possuiNeurodiversidade).click()
+    cy.get(formStep.opcoesNeurdiversidade).eq(dados.possuiNeurodiversidade).click()
     
-    if (cadastro.possuiNeurodiversidade) {
-      cy.get(formStep.campoNeurodivergenciaDescricao).type(cadastro.neurodiversidade)
+    if (dados.possuiNeurodiversidade) {
+      cy.get(formStep.campoNeurodivergenciaDescricao).type(dados.neurodiversidade)
     }  
 })
 
 
+Cypress.Commands.add("preencherTodosOsCampos", (dados) => {
+  if (dados.nome) cy.get(formStep.campoNome).type(dados.nome)
+  if (dados.email) cy.get(formStep.campoEmail).type(dados.email)
+  if (dados.rg) cy.get(formStep.campoRg).type(dados.rg)
+  if (dados.cpf) cy.get(formStep.campoCpf).type(dados.cpf)
+  if (dados.telefone) cy.get(formStep.campoTelefone).type(dados.telefone)
+  if (dados.dataNascimento) cy.get(formStep.campoDataNascimento).type(dados.dataNascimento)
+  if (dados.cidade) cy.get(formStep.campoCidade).type(dados.cidade)
+
+  cy.get(formStep.campoEstado).click()
+  cy.get(formStep.opcoesEstado).eq(dados.estado).click()
+
+  cy.get(formStep.campoNeurodiversidade).click()
+  cy.get(formStep.opcoesNeurdiversidade).eq(dados.possuiNeurodiversidade).click()
+  
+  if (dados.possuiNeurodiversidade) {
+    cy.get(formStep.campoNeurodivergenciaDescricao).type(dados.neurodiversidade)
+  }
+})
+
 Cypress.Commands.add("clicarEmProximo", () => {
   cy.get(formStep.btnProximo).click()
+})
+
+Cypress.Commands.add("validarSeEstaNaSegundaEtapa", () => {
+  cy.get(formStep.btnVoltar).should('have.text', 'Voltar')
+})
+
+Cypress.Commands.add("validarMensagemDeErro", (mensagem, seletor) => {
+  cy.get(seletor).should('have.text', mensagem)
 })
