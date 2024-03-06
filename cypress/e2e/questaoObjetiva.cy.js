8d0adbe68bcf72daaad302a3326d9dd112a1f5d6
Luiz Fellipe de Almeida R. Barbosa <luizfellipedearb@gmail.com>
2024-03-06 17:50:41 -0300

    add adicionando mais tests cases questão objetiva

--- cypress/e2e/questaoObjetiva.cy.js
+++ cypress/e2e/questaoObjetiva.cy.js
@@ -2,40 +2,50 @@
 
 // TESTES FALHANDO DEVIDO ERROS NÃO TRATADOS NO FRONT-END
 describe('Testes de Questão Objetiva', () => {
-    beforeEach(() => {
-      cy.visit("/login").wait(3000)
-      Cypress.on('uncaught:exception', (err, runnable) => {
-        // retorna false para evitar que o Cypress falhe o teste
-        return false;
+  beforeEach(() => {
+    cy.visit("/login").wait(3000)
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      // retorna false para evitar que o Cypress falhe o teste
+      return false;
     });
   })
-  
-    it('Test 1 - Criação de Questão Objetiva com Sucesso', () => {
-        cy.fixture("login").then(({ techlead }) => {
-            cy.preencherCamposDeLogin(techlead)
-            cy.clicarEmLogin()  
-            cy.validarLogin("Olá, instrutor teste")
-          })
-        cy.criarQuestaoObjetivaComSucesso()
-    })
 
-    it('Test 2 - Criação de Questão Objetiva sem preencher campos obrigatórios', () => {
-      cy.fixture("login").then(({ techlead }) => {
-          cy.preencherCamposDeLogin(techlead)
-          cy.clicarEmLogin()  
-          cy.validarLogin("Olá, instrutor teste")
-        })
-        cy.criarQuestaoObjetivaSemSucesso()
+  it('Test 1 - Criação de Questão Objetiva com Sucesso', () => {
+    cy.fixture("login").then(({ techlead }) => {
+      cy.preencherCamposDeLogin(techlead)
+      cy.clicarEmLogin()
+      cy.validarLogin("Olá, instrutor teste")
+    })
+    cy.criarQuestaoObjetivaComSucesso()
   })
 
-  it.only('Test 3 - Remover Questão Objetiva com sucesso', () => {
+  it('Test 2 - Criação de Questão Objetiva sem preencher campos obrigatórios', () => {
     cy.fixture("login").then(({ techlead }) => {
-        cy.preencherCamposDeLogin(techlead)
-        cy.clicarEmLogin()  
-        cy.validarLogin("Olá, instrutor teste")
-      })
-      cy.removerQuestãoSucesso()
-  
+      cy.preencherCamposDeLogin(techlead)
+      cy.clicarEmLogin()
+      cy.validarLogin("Olá, instrutor teste")
+    })
+    cy.criarQuestaoObjetivaSemSucesso()
+  })
+
+  it('Test 3 - Remover Questão Objetiva com sucesso', () => {
+    cy.fixture("login").then(({ techlead }) => {
+      cy.preencherCamposDeLogin(techlead)
+      cy.clicarEmLogin()
+      cy.validarLogin("Olá, instrutor teste")
+    })
+    cy.removerQuestãoSucesso()
+
+  })
+
+  it.only('Test 4 - Editar Questão Objetiva com sucesso', () => {
+    cy.fixture("login").then(({ techlead }) => {
+      cy.preencherCamposDeLogin(techlead)
+      cy.clicarEmLogin()
+      cy.validarLogin("Olá, instrutor teste")
+    })
+    cy.editarQuestãoComSucesso()
+
   })
 
 })
