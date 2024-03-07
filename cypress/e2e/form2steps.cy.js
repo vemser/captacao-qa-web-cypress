c6c636e6621336b069bd1a74c536e5b931381635
Aron Adams <aronrapetto@hotmail.com>
2024-03-06 21:12:15 -0300

    fix: caminho de arquivos no form 2

--- cypress/e2e/form2steps.cy.js
+++ cypress/e2e/form2steps.cy.js
@@ -2,7 +2,7 @@
 
 
 
-describe(' Form 2 Steps', () => {
+describe('Form 2 Steps', () => {
     beforeEach(() => {
       cy.allure().feature("Segundo formulário de cadastro do candidato")
       cy.allure().severity('Crítico')
@@ -21,7 +21,7 @@ describe(' Form 2 Steps', () => {
       cy.botaoEnviar()
     })
 
-    it.only('Test 2 - Validar Preenchimento sem colocar a Instituição', () => {
+    it('Test 2 - Validar Preenchimento sem colocar a Instituição', () => {
       cy.preencherTodosCamposComSucesso()
       cy.clicarEmProximo()
       cy.preencherTodosCamposComSucesso2()
@@ -30,7 +30,7 @@ describe(' Form 2 Steps', () => {
       cy.get('#instituicao-de-ensino-candidato-helper-text').should('contain', 'A Instituição de ensino deve ter apenas letras,espaços ou pontuações')
     })
 
-    it.only('Test 3 - Validar Preenchimento sem preencher curso', () => {
+    it('Test 3 - Validar Preenchimento sem preencher curso', () => {
       cy.preencherTodosCamposComSucesso()
       cy.clicarEmProximo()
       cy.preencherTodosCamposComSucesso2()
@@ -39,7 +39,7 @@ describe(' Form 2 Steps', () => {
       cy.get('#curso-candidato-helper-text').should('contain', 'O Curso deve ter apenas letras,espaços ou pontuações')
     })
 
-    it.only('Test 4 - Validar Preenchimento sem responder a pergunta: Por qual o outro motivo voce se interessou ela area de tecnologia?', () => {
+    it('Test 4 - Validar Preenchimento sem responder a pergunta: Por qual o outro motivo voce se interessou ela area de tecnologia?', () => {
       cy.preencherTodosCamposComSucesso()
       cy.clicarEmProximo()
       cy.preencherTodosCamposComSucesso2()
@@ -48,7 +48,7 @@ describe(' Form 2 Steps', () => {
       cy.get('#s2-candidato-motivo-helper-text').should('contain', 'Campo obrigatório')
     })
 
-    it.only('Test 5 - Validar Preenchimento sem responder a pergunta: Alguem te ensinou algo importante...?', () => {
+    it('Test 5 - Validar Preenchimento sem responder a pergunta: Alguem te ensinou algo importante...?', () => {
       cy.preencherTodosCamposComSucesso()
       cy.clicarEmProximo()
       cy.preencherTodosCamposComSucesso2()
@@ -57,7 +57,7 @@ describe(' Form 2 Steps', () => {
       cy.get('#textarea-importante-candidato-helper-text').should('contain', 'Campo obrigatório')
     })
 
-    it.only('Test 6 - Validar sem selecionar a linguagem', () => {
+    it('Test 6 - Validar sem selecionar a linguagem', () => {
       cy.preencherTodosCamposComSucesso()
       cy.clicarEmProximo()
       cy.preencherSemSelecionarLinguagem()
@@ -65,7 +65,7 @@ describe(' Form 2 Steps', () => {
       cy.get('.MuiFormHelperText-root').should('contain', 'Pelo menos uma linguagem deve ser selecionada')
     })
 
-    it.only('Test 7 - Validar sem selecionar a trilha', () => {
+    it('Test 7 - Validar sem selecionar a trilha', () => {
       cy.preencherTodosCamposComSucesso()
       cy.clicarEmProximo()
       cy.preencherSemSelecionarTrilha()
@@ -73,7 +73,7 @@ describe(' Form 2 Steps', () => {
       cy.get('#root > div.MuiBox-root.css-uqekie > div > div > div > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.css-620t2 > form > div:nth-child(12) > p').should('contain', 'A escolha de uma trilha é obrigatória')
     })
 
-    it.only('Test 8 - Validar sem adicionar curriculo', () => {
+    it('Test 8 - Validar sem adicionar curriculo', () => {
       cy.preencherTodosCamposComSucesso()
       cy.clicarEmProximo()
       cy.preencherSemAdicionarCurriculo()
@@ -81,7 +81,7 @@ describe(' Form 2 Steps', () => {
       cy.get('.MuiFormHelperText-root').should('contain', 'O currículo é obrigatório');
     });
 
-    it.only('Test 9 - Validar sem adicionar print configuração computador', () => {
+    it('Test 9 - Validar sem adicionar print configuração computador', () => {
       cy.preencherTodosCamposComSucesso()
       cy.clicarEmProximo()
       cy.preencherSemAdicionarPrintPC()
@@ -89,7 +89,7 @@ describe(' Form 2 Steps', () => {
       cy.get('.MuiFormHelperText-root').should('contain', 'O print é obrigatório');
     });
 
-    it.only('Test 10 - Validar sem selecionar: Você concorda com o tratamento dos seus dados pessoais...', () => {
+    it('Test 10 - Validar sem selecionar: Você concorda com o tratamento dos seus dados pessoais...', () => {
       cy.preencherTodosCamposComSucesso()
       cy.clicarEmProximo()
       cy.SemSelecionarQueConcorda()
@@ -97,28 +97,28 @@ describe(' Form 2 Steps', () => {
       cy.get('#mensagem-erro-lgpd').should('contain', 'É necessário aceitar os termos para prosseguir');
     });
 
-    it.only('Test 11 - Validar selecionando a matriculado em curso de graducao: nao', () => {
+    it('Test 11 - Validar selecionando a matriculado em curso de graducao: nao', () => {
       cy.preencherTodosCamposComSucesso()
       cy.clicarEmProximo()
       cy.get('#matriculado-nao > .MuiButtonBase-root > .PrivateSwitchBase-input').click()
       cy.get('#erro-não-matriculado').should('contain', 'Devido as restrições impostas pelas leis brasileiras, somente alunos que possuem vínculo com uma instituição de ensino podem se candidatar às vagas de estágio.')
     })
 
-    it.only('Test 12 - Validar sem selecionando nenhum nivel de ingles', () => {
+    it('Test 12 - Validar sem selecionando nenhum nivel de ingles', () => {
       cy.preencherTodosCamposComSucesso()
       cy.clicarEmProximo()
       cy.semSelecionarNenhumNivelIngles()
       cy.botaoEnviar()
     });
 
-    it.only('Test 13 - Validar sem selecionando nenhum nivel de espanhol', () => {
+    it('Test 13 - Validar sem selecionando nenhum nivel de espanhol', () => {
       cy.preencherTodosCamposComSucesso()
       cy.clicarEmProximo()
       cy.semSelecionarNenhumNivelEspanhol()
       cy.botaoEnviar()
     });
 
-    it.only('Test 14 - Validar preenchendo "motivo" menos de 10 caracteres', () => {
+    it('Test 14 - Validar preenchendo "motivo" menos de 10 caracteres', () => {
       cy.preencherTodosCamposComSucesso()
       cy.clicarEmProximo()
       cy.preencherMotivoMenos10Caracteres()
@@ -126,7 +126,7 @@ describe(' Form 2 Steps', () => {
       cy.get('#s2-candidato-motivo-helper-text').should('contain', 'São necessários 10 caracteres, no mínimo')
     });
 
-    it.only('Test 15 - Validar preenchendo "ensinamento" menos de 10 caracteres', () => {
+    it('Test 15 - Validar preenchendo "ensinamento" menos de 10 caracteres', () => {
       cy.preencherTodosCamposComSucesso()
       cy.clicarEmProximo()
       cy.preencherEnsinoMenos10Caracteres()
@@ -134,7 +134,7 @@ describe(' Form 2 Steps', () => {
       cy.get('#textarea-importante-candidato-helper-text').should('contain', 'São necessários 10 caracteres, no mínimo')
     });
 
-    it.only('Test 16 - Validar preenchendo numero no campo instituicao', () => {
+    it('Test 16 - Validar preenchendo numero no campo instituicao', () => {
       cy.preencherTodosCamposComSucesso()
       cy.clicarEmProximo()
       cy.preencherNumeroCampoInstituicao()
@@ -142,7 +142,7 @@ describe(' Form 2 Steps', () => {
       cy.get('#instituicao-de-ensino-candidato-helper-text').should('contain', 'A Instituição de ensino deve ter apenas letras,espaços ou pontuações')
     });
 
-    it.only('Test 17 - Validar preenchendo instituicao mais de 80 caracteres', () => {
+    it('Test 17 - Validar preenchendo instituicao mais de 80 caracteres', () => {
       cy.preencherTodosCamposComSucesso()
       cy.clicarEmProximo()
       cy.preenchendoInstituicaoMais80Caracteres()
